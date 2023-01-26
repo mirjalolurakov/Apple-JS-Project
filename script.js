9e97992ff90a3a9c04e9a4c77afbdb47ebfc870d
Urakov Mirjalol <mirjalolurakov19@gmail.com>
2023-01-26 03:37:43 +0000

    completed

---
+++ script.js
@@ -0,0 +1,17 @@
+// let btn = document.querySelectoAll('button')
+
+
+// btns.forEach((btn) => {
+//     btn.onclick = () => {
+//         if(btn. === "Purple"){
+//             src.setAttribute('src' ,'./')
+//         }else{
+//             img.setAttribute('src', './')
+//         }
+
+//     }
+
+   
+// });
+
+
